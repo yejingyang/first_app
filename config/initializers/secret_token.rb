640c78ec74bcd417f5a8166a29ed94a32cef6913
yang <jyangye@163.com>
2013-11-16 21:53:33 +0800

    Add a static page contraller

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -15,7 +15,7 @@ require 'securerandom'
 def secure_token
 	token_file =  Rails.root.join('.secret')
 	if File.exist?(token_file)
-		File.read(token_file.chomp)
+		File.read(token_file).chomp
 	else
 		token = SecureRandom.hex(64)
 		File.write(token_file, token)
