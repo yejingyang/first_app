c6e1d211b25b4953a3bf130a2119de0ce15ce790
yang <jyangye@163.com>
2013-11-17 20:43:07 +0800

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ FirstApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by yejingyang
+  ActiveModel::SecurePassword.min_cost = true
 end
